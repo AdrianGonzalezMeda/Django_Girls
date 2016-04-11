f5ea93e55d1824e8c88061abc4ee6eb49da2e0b4
AdrianGonzalezMeda <adriangonzalezmeda@gmail.com>
2016-04-11 10:34:08 +0200

    Cambio a msql en dev

--- django_girls/settings.py
+++ django_girls/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'django_girls.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST' : 'localhost',
+        'NAME' : 'django_girls',
+        'USER' : 'root',
+        'PASSWORD' : 'root',
+        'ATOMIC_REQUESTS' : True,
     }
 }
 
