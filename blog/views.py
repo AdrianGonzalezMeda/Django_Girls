2444e11b688b90821821206148e6c22fc773cf9d
AdrianGonzalezMeda <adriangonzalezmeda@gmail.com>
2016-04-11 12:38:11 +0200

    Segundo commit dev

--- blog/views.py
+++ blog/views.py
@@ -2,17 +2,33 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.shortcuts import redirect
 
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {"posts" : posts})
+    return render(request, 'blog/post_list.html', {'posts' : posts}) #Lo que pasas como contexto tienes que recogerlo luego en el html con el mismo nombre que lo que pones entre comillas
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {"post" : post})
+    comments = Comment.objects.filter(post=post).order_by('created_date')
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit = False)#Variable distinta a la de arriba
+            comment.author = request.user
+            comment.post = post #Lo iguala al post que recogemos arriba, que va a ser uno solo
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog/post_detail.html', {'post' : post,
+                                                    'comments' : comments,
+                                                    'form' : form})
 
 def post_new(request):
     if request.method == "POST":
@@ -22,10 +38,10 @@ def post_new(request):
             post = form.save(commit = False)#Esto simplemente te lo guarda en una variable antes de "subirlo" por que antes queremos anadirle el autor y eso.
             post.author = request.user
             post.publish()#metodo propio, lo publica y le pone fecha de publicacion
-            return redirect('blog.views.post_detail', pk=post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html', {"form" : form})
+    return render(request, 'blog/post_edit.html', {'form' : form})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -39,4 +55,4 @@ def post_edit(request, pk):
             return redirect('blog.views.post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {"form" : form})
+    return render(request, 'blog/post_edit.html', {'form' : form})
