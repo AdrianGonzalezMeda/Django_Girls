2ad8d840857d1df421613eba2c79842cf1f0a2bb
AdrianGonzalezMeda <adriangonzalezmeda@gmail.com>
2016-04-15 10:27:25 +0200

    Añadido comentario en settings.py

--- django_girls/settings.py
+++ django_girls/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
 )
 
-ROOT_URLCONF = 'django_girls.urls'
+ROOT_URLCONF = 'django_girls.urls' #Configuracion de urls base
 
 TEMPLATES = [
     {
