b4db993b24619dfefabcd1ac370fa797f01f9872
AdrianGonzalezMeda <adriangonzalezmeda@gmail.com>
2016-04-18 12:22:26 +0200

    Añadido Comments al CBV de PostDetail

--- blog/forms.py
+++ blog/forms.py
@@ -2,12 +2,13 @@ from django import forms
 from .models import Post, Comment
 
 
-class PostForm(forms.ModelForm):
+# class PostForm(forms.ModelForm):  Ya no nos sirve de nada por que se crea directamente en las class based view, que heredan de model form mixins que es quien
+#le da el comportamiento de crear formiularios basados en un modelo
 
 
-	class Meta:
-		model = Post
-		fields = ('title', 'text',)
+# 	class Meta:
+# 		model = Post
+# 		fields = ('title', 'text',)
 
 class CommentForm(forms.ModelForm):
 
--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.core.urlresolvers import reverse
 
 from .models import Post, Comment
-from .forms import PostForm, CommentForm
+from .forms import CommentForm
 
 
 # Create your views here.
@@ -58,6 +58,14 @@ class PostList(ListView):
 #                                                     'form' : form})
 class PostDetail(DetailView):
     model = Post
+    fields = ['title', 'text']
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(PostDetail, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['comments'] = Comment.objects.filter(post=self.object).order_by('created_date') #Filtra que salgan los comentarios del post en el que estan referenciados
+        return context
 
 
 
